1e5c3856beb6d4d3500fa0a52dc9b5f4f1651a16
Aliaksandr_Sauchanka <Aliaksandr_Sauchanka@DESKTOP-CBU869C.local>
2020-02-22 16:13:36 +0200

    token

--- ProPetsAccount/src/main/java/propets/account/controller/AccountController.java
+++ ProPetsAccount/src/main/java/propets/account/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,50 +33,50 @@ public class AccountController {
 	}
 	
 	@PostMapping("/login")
-	public UserDto loginUser(@RequestBody String token) {
+	public UserDto loginUser(@RequestHeader("Authorization") String token) {
 		return accountService.loginUser(token);
 	}
 	
 	@GetMapping("/{login}/info")
-	public UserDto userInformation(@PathVariable String login, String token) {
+	public UserDto userInformation(@PathVariable String login, @RequestHeader("Authorization") String token) {
 		return accountService.userInformation(login, token);
 	}
 	
 	@PutMapping("/{login}")
-	public UserDto editUserProfile(@RequestBody EditUserDto editUser, @PathVariable String login, String token) {
+	public UserDto editUserProfile(@RequestBody EditUserDto editUser, @PathVariable String login, @RequestHeader("Authorization") String token) {
 		return accountService.editUserProfile(editUser, login, token);
 	}
 	
 	@DeleteMapping("/{login}")
-	public UserDto removeUser(@PathVariable String login, String token) {
+	public UserDto removeUser(@PathVariable String login, @RequestHeader("Authorization") String token) {
 		return accountService.removeUser(login, token);
 	}
 	
 	@PutMapping("/{login}/roles")
 	//Nado vozvraschat' UserDto
-	public Set<String> addUserRoles(@PathVariable String login, @RequestBody String role, String token) {
+	public Set<String> addUserRoles(@PathVariable String login, @RequestBody String role, @RequestHeader("Authorization") String token) {
 		//access permitted only to admin
 		return accountService.addRoles(login, role, token);
 	}
 	
 	@PutMapping("/{login}/block/{status}")
-	public UserDto blockUserAccount(@PathVariable String login, @RequestBody BlockUserDto blockUser, String token, @PathVariable boolean status) {
+	public UserDto blockUserAccount(@PathVariable String login, @RequestBody BlockUserDto blockUser, @RequestHeader("Authorization") String token, @PathVariable boolean status) {
 		return accountService.blockUserAccount(login, blockUser, token, status);
 	}
 	
 	@PutMapping("/{login}/favorite/{id}")
-	public Set<String> addUserFavorite(@PathVariable String login, @PathVariable String id, String token) {
+	public Set<String> addUserFavorite(@PathVariable String login, @PathVariable String id, @RequestHeader("Authorization") String token) {
 		return accountService.addUserFavorite(login, id, token);
 	}
 	
 	@DeleteMapping("/{login}/favorite/{id}")
 	//nado vozvr... 1 ID, a ne Iterable
-	public Set<String> removeUserFavorite(@PathVariable String login, @PathVariable String id, String token) {
+	public Set<String> removeUserFavorite(@PathVariable String login, @PathVariable String id, @RequestHeader("Authorization") String token) {
 		return accountService.removeUserFavorite(login, id, token);
 	}
 	
 	@GetMapping("/{login}/favorites")
-	public Set<String> getUserFavorites(@PathVariable String login, String token){
+	public Set<String> getUserFavorites(@PathVariable String login, @RequestHeader("Authorization") String token){
 		return accountService.getUserFavorites(login, token);
 	}
 	
