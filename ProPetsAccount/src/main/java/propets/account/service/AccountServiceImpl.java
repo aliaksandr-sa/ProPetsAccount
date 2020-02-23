623b1dba9859ef646ccf2cf345861fde022e38c9
Aliaksandr_Sauchanka <Aliaksandr_Sauchanka@DESKTOP-CBU869C.local>
2020-02-23 21:19:19 +0200

    remove favor

--- ProPetsAccount/src/main/java/propets/account/service/AccountServiceImpl.java
+++ ProPetsAccount/src/main/java/propets/account/service/AccountServiceImpl.java
@@ -139,7 +139,7 @@ public class AccountServiceImpl implements AccountService {
 		if (!email.equals(login)) {
 			throw new ConflictException();
 		}
-		User user = accountRepository.findById(login).get();
+		User user = accountRepository.findById(login).orElseThrow(() -> new ConflictException());
 		user.removeFavorite(id);
 		accountRepository.save(user);
 		return user.getFavoritePosts();
